cfcb206e60682c062b3a408449aa638054148e80
Saurabh Pandey <sp.saurabh.007b@gmail.com>
2026-04-04 01:17:25 +0530

    adding repos from local to git

---
+++ springBoot-trial/src/main/java/com/example/main/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.example.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
+})
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
